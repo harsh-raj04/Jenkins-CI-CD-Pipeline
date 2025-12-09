fd4b1b7cd799a0c606e9eb2daee7c5d96ab65c2f
harsh-raj04 <25042002harsh@gmail.com>
2025-12-09 11:48:25 +0530

    Opened the 9100 port for node expoter

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -93,6 +93,13 @@ resource "aws_security_group" "web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Node Exporter"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     description = "Allow all outbound"
